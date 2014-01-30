9d49e70a352b0a99f8a783cdd272e53d7ed6b604
Benjamin Hutchins <ben@hutchins.co>
2014-01-30 01:55:46 -0500

    Changed out legacy (and outdated) Mocha UI slider for more modern HTML5 <input type="range"> to fix qbittorrent/qBittorrent#1036 ; This is not globally supported and therefore I've also mimiced the qBittorrent's desktop UI and added a generic input which is always available. This also allows the setting to be placed outside the range of the <input type="range">(fix qbittorrent/qBittorrent#43)

--- src/webui/scripts/parametrics.js
+++ src/webui/scripts/parametrics.js
@@ -1,191 +1,78 @@
-/*
+(function () {
+	function supportsRange() {
+		var i = document.createElement('input');
+		i.type = 'range';
+		return i.type !== 'text';
+	}
 
-Script: Parametrics.js
-	Initializes the GUI property sliders.
-
-Copyright:
-	Copyright (c) 2007-2008 Greg Houston, <http://greghoustondesign.com/>.	
-
-License:
-	MIT-style license.
-
-Requires:
-	Core.js, Window.js
-
-*/
-
-MochaUI.extend({
-	addUpLimitSlider: function(hash){
-		if ($('uplimitSliderarea')) {
-			var windowOptions = MochaUI.Windows.windowOptions;
-			var sliderFirst = true;
-			// Get global upload limit
-			var maximum = 500;
-			var req = new Request({
-				url: 'command/getGlobalUpLimit',
+	MochaUI.extend({
+		// direction can be "Up" or "Dl""
+		// hash is either "global" or torrent hash identifier
+		setupSlider: function (direction, hash) {
+			// Get global rate limit
+			new Request({
+				url: 'command/getGlobal' + direction + 'Limit',
 				method: 'post',
-				data: {},
-				onSuccess: function(data) {
-					if(data){
-						var tmp = data.toInt();
-						if(tmp > 0) {
-							maximum = tmp / 1024.
-						} else {
-							maximum = 0
+				onSuccess: function (data) {
+					// get the global maximum, defaulting to 0 (infinite)
+					var maximum = data ? parseInt(data, 10) : 0;
+					if (maximum > 0) {
+						maximum = Math.ceil(maximum / 1024);
+					}
+
+					if (hash == 'global') {
+						var limit = Number.max(0, maximum);
+						maximum = direction === 'Up' ? 1000 : 10000;
+
+						if (supportsRange()) {
+							new Element('input', {
+								type: 'range',
+								min: 0,
+								max: maximum,
+								step: 10,
+								value: Math.round(limit / 1024)
+							}).addEvent('change', function () {
+								var pos = this.get('value');
+								$('limitValue').set('value', pos > 0 ? pos + ' _(KiB/s)' : '∞');
+							});
 						}
+
+						// Set default value
+						$('limitValue').set('value', limit === 0 ? '∞' : Math.round(limit / 1024) + ' _(KiB/s)');
 					}
 					// Get torrent upload limit
-					// And create slider
-					if(hash == 'global') {
-						var up_limit = maximum;
-						if(up_limit < 0) up_limit = 0;
-						maximum = 1000;
-						var mochaSlide = new Slider($('uplimitSliderarea'), $('uplimitSliderknob'), {
-										steps: maximum,
-										offset: 0,
-										initialStep: up_limit.round(),
-										onChange: function(pos){
-											if(pos > 0) {
-												$('uplimitUpdatevalue').set('html', pos);
-												$('upLimitUnit').set('html', "_(KiB/s)");
-											} else {
-												$('uplimitUpdatevalue').set('html', '∞');
-												$('upLimitUnit').set('html', "");
-											}
-										}.bind(this)
-									});
-						// Set default value
-						if(up_limit == 0) {
-							$('uplimitUpdatevalue').set('html', '∞');
-							$('upLimitUnit').set('html', "");
-						} else {
-							$('uplimitUpdatevalue').set('html', up_limit.round());
-							$('upLimitUnit').set('html', "_(KiB/s)");
-						}
-					} else {
-						var req = new Request({
-							url: 'command/getTorrentUpLimit',
+					else {
+						new Request({
+							url: 'command/getTorrent' + direction + 'Limit',
 							method: 'post',
-							data: {hash: hash},
+							data: {
+								hash: hash
+							},
 							onSuccess: function(data) {
-								if(data){
-									var up_limit = data.toInt();
-									if(up_limit < 0) up_limit = 0;
-									var mochaSlide = new Slider($('uplimitSliderarea'), $('uplimitSliderknob'), {
-										steps: maximum,
-										offset: 0,
-										initialStep: (up_limit/1024.).round(),
-										onChange: function(pos){
-											if(pos > 0) {
-												$('uplimitUpdatevalue').set('html', pos);
-												$('upLimitUnit').set('html', "_(KiB/s)");
-											} else {
-												$('uplimitUpdatevalue').set('html', '∞');
-												$('upLimitUnit').set('html', "");
-											}
-										}.bind(this)
+								var limit = data ? Math.max(0, parseInt(data, 10)) : 0;
+
+								if (supportsRange()) {
+									var range = new Element('input', {
+										type: 'range',
+										min: 0,
+										max: maximum === 0 ? (direction === 'Up' ? 1000 : 10000) : maximum,
+										step: 10,
+										value: Math.round(limit / 1024)
+									}).addEvent('change', function (event) {
+										var pos = this.get('value');
+										$('limitValue').set('value', pos > 0 ? pos + ' _(KiB/s)' : '∞');
 									});
-									// Set default value
-									if(up_limit == 0) {
-										$('uplimitUpdatevalue').set('html', '∞');
-										$('upLimitUnit').set('html', "");
-									} else {
-										$('uplimitUpdatevalue').set('html', (up_limit/1024.).round());
-										$('upLimitUnit').set('html', "_(KiB/s)");
-									}
+
+									$$('.sliderWrapper').adopt(range);
 								}
+
+								// Set default value
+								$('limitValue').set('value', limit === 0 ? '∞' : Math.round(limit / 1024) + ' _(KiB/s)');
 							}
 						}).send();
 					}
 				}
 			}).send();
 		}
-	},
-	
-	addDlLimitSlider: function(hash){
-		if ($('dllimitSliderarea')) {
-			var windowOptions = MochaUI.Windows.windowOptions;
-			var sliderFirst = true;
-			// Get global upload limit
-			var maximum = 500;
-			var req = new Request({
-				url: 'command/getGlobalDlLimit',
-				method: 'post',
-				data: {},
-				onSuccess: function(data) {
-					if(data){
-						var tmp = data.toInt();
-						if(tmp > 0) {
-							maximum = tmp / 1024.
-						} else {
-							maximum = 0
-						}
-					}
-					// Get torrent download limit
-					// And create slider
-					if(hash == "global") {
-						var dl_limit = maximum;
-						if(dl_limit < 0) dl_limit = 0;
-						maximum = 10000;
-						var mochaSlide = new Slider($('dllimitSliderarea'), $('dllimitSliderknob'), {
-							steps: maximum,
-							offset: 0,
-							initialStep: dl_limit.round(),
-							onChange: function(pos){
-								if(pos > 0) {
-									$('dllimitUpdatevalue').set('html', pos);
-									$('dlLimitUnit').set('html', "_(KiB/s)");
-								} else {
-									$('dllimitUpdatevalue').set('html', '∞');
-									$('dlLimitUnit').set('html', "");
-								}
-							}.bind(this)
-						});
-						// Set default value
-						if(dl_limit == 0) {
-							$('dllimitUpdatevalue').set('html', '∞');
-							$('dlLimitUnit').set('html', "");
-						} else {
-							$('dllimitUpdatevalue').set('html', dl_limit.round());
-							$('dlLimitUnit').set('html', "_(KiB/s)");
-						}
-					} else {
-						var req = new Request({
-							url: 'command/getTorrentDlLimit',
-							method: 'post',
-							data: {hash: hash},
-							onSuccess: function(data) {
-								if(data){
-									var dl_limit = data.toInt();
-									if(dl_limit < 0) dl_limit = 0;
-									var mochaSlide = new Slider($('dllimitSliderarea'), $('dllimitSliderknob'), {
-										steps: maximum,
-										offset: 0,
-										initialStep: (dl_limit/1024.).round(),
-										onChange: function(pos){
-											if(pos > 0) {
-												$('dllimitUpdatevalue').set('html', pos);
-												$('dlLimitUnit').set('html', "_(KiB/s)");
-											} else {
-												$('dllimitUpdatevalue').set('html', '∞');
-												$('dlLimitUnit').set('html', "");
-											}
-										}.bind(this)
-									});
-									// Set default value
-									if(dl_limit == 0) {
-										$('dllimitUpdatevalue').set('html', '∞');
-										$('dlLimitUnit').set('html', "");
-									} else {
-										$('dllimitUpdatevalue').set('html', (dl_limit/1024.).round());
-										$('dlLimitUnit').set('html', "_(KiB/s)");
-									}
-								}
-							}
-						}).send();
-					}
-				}
-			}).send();
-		}
-	}
-});
+	});
+})();
